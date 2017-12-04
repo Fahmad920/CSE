fa3f28175f3711a08ad30dd113ac583de8a38014
Fahmad920 <31050480+Fahmad920@users.noreply.github.com>
2017-12-04 15:39:57 -0800

    Warmups
    12/4/17

--- Fatima- GuessGame.py
+++ Fatima- GuessGame.py
@@ -1,5 +1,5 @@
 import random
-# print(random.randint(0, 50))
+#print(random.randint(0, 50))
 
 
 number = input("Guess a Number ")
@@ -14,3 +14,12 @@ if number == 25:
 #         print("Correct")
 #     elif number != 25:
 #         print("Guess Again")
+
+
+# github.com/MrWiebe/CSE7
+# 1) get computer to generate a random number 1-50
+# 2) get a number (input) from the user
+# 3) compare number to input
+# 4) Add "Higher" or "lower"
+# 5) add 5 guesses
+# for guesses, remove one
---
+++ Warmups.py
@@ -0,0 +1,21 @@
+# 12.4.17
+
+
+# def reverse_order(first_name, last_name):
+#     print("%s %s" % (last_name, first_name))
+
+
+def reverse_order():
+    first_name = input("What is you first name?")
+    last_name = input("What is you last name?")
+    print(last_name + " " + first_name)
+
+
+def happy_bday(name):
+    print("Happy birthday to you,")
+    print("Happy birthday to you,")
+    print("Happy birthday dear %s" % name)
+    print("Happy birthday to you!")
+
+
+
