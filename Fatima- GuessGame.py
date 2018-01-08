f84cf2d1d006651efae93b6e289319d176786c05
Fahmad920 <31050480+Fahmad920@users.noreply.github.com>
2018-01-08 15:40:58 -0800

    New Lesson

--- Fatima- GuessGame.py
+++ Fatima- GuessGame.py
@@ -1,8 +1,12 @@
 import random
+
+# Initializing Variables
 answer = random.randint(1, 50)
 turn = 5
 correct_guess = False
 
+
+# Describes exactly ONE turn. The while loop is the Game Controller.
 while turn > 0 and correct_guess == False:
     guess = int(input("Guess a Number 1-50 "))
     if guess == answer:
@@ -17,3 +21,12 @@ while turn > 0 and correct_guess == False:
 
 if turn == 0:
     print("Good Job, but that was not the answer. The correct answer was %s" % answer)
+
+# Lucky 7s structure
+# two dice
+# if rolled 7 then you win money
+# if roll anything else then lost money
+# every round bet $1
+# win = bet $1 + additional $4
+# start with $15
+#
