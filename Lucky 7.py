dcdfa7c6b1d5b5629600f03c53705534bbe9bfc0
Fahmad920 <31050480+Fahmad920@users.noreply.github.com>
2018-01-09 15:42:27 -0800

    1/9/17

--- Lucky 7.py
+++ Lucky 7.py
@@ -2,9 +2,13 @@ import random
 D1 = int(random.randint(1, 6))
 D2 = int(random.randint(1, 6))
 print(D1 + D2)
+seven = 7
 
 
 money = 15
 bet = 1
-print("You have %s dollars" % money)
+while money > 0:
+    print("You have %s money" % money)
 
+if D1 + D2 == seven:
+    money += 5
