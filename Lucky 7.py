9707da2e43e7c319715e2e2bf8ea1a5561779d54
Fahmad920 <31050480+Fahmad920@users.noreply.github.com>
2018-01-11 15:39:49 -0800

    1/11/18

--- Lucky 7.py
+++ Lucky 7.py
@@ -4,20 +4,26 @@ money = 15
 bet = 1
 MaxRound = 0
 HighMoney = 0
-Round = 0
+HighRound = 0
 while money > 0:
-    print("You have %s dollar" % money)
+    print("You have $%s" % money)
     D1 = int(random.randint(1, 6))
     D2 = int(random.randint(1, 6))
     print(D1 + D2)
     if D1 + D2 == seven:
         money += 4
+        if HighMoney > money:
+            HighMoney = money
     elif D1 + D2 != seven:
         money -= 1
     MaxRound += 1
+    if HighRound > MaxRound:
+        HighRound = MaxRound
 
 if money == 0:
     print("You played %s rounds." % MaxRound)
-    print("You lost all your money");
+    print("You lost all your money")
 
-if HighMoney == MaxRound:
+if HighMoney == HighRound:
+    print("Highest amount of money was $%s" % HighMoney)
+    print("You had the most money at %s round" % HighRound)
