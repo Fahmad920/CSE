dbcf3bb34232b8691501b829f17734499c3a2272
Fahmad920 <31050480+Fahmad920@users.noreply.github.com>
2018-01-30 15:38:55 -0800

    almost complete hangman

--- HangMan.py
+++ HangMan.py
@@ -8,18 +8,29 @@ guesses_left = 10
 words = random.choices(hidden_words)
 newStr = ''.join(words)
 listOne = list(newStr)
-print(newStr)
 letter = string. ascii_letters
 
 
 while guesses_left > 0:
+    output = []
     for letter in newStr:
         if letter in letters_guessed:
-            #else:
-            # hide letters
-            their_guess = input("Guess a letter ")
+            output.append(letter)
+        else:
+            output.append("*")
+    print(output)
+
+    their_guess = input("Guess a letter ")
+    letters_guessed.append(their_guess)
     if their_guess in newStr:
         print("You guessed it right!")
     else:
         guesses_left -= 1
         print("You are wrong!")
+
+if guesses_left == 0:
+    print("The word was %s" % newStr)
+
+if their_guess == newStr:
+    guesses_left = 0
+    print("You guessed the word. The word was %s" % newStr)
