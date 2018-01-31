cad61ff42fc7c748bdf8bc5d3eea30b6451adff5
Fahmad920 <31050480+Fahmad920@users.noreply.github.com>
2018-01-31 15:27:31 -0800

    1/31/18

--- HangMan.py
+++ HangMan.py
@@ -13,24 +13,29 @@ letter = string. ascii_letters
 
 while guesses_left > 0:
     output = []
+
     for letter in newStr:
         if letter in letters_guessed:
             output.append(letter)
         else:
             output.append("*")
-    print(output)
+    str1 = ''.join(output)
+    print(str1)
 
     their_guess = input("Guess a letter ")
     letters_guessed.append(their_guess)
     if their_guess in newStr:
         print("You guessed it right!")
+        if guesses_left > 0 and their_guess == words:
+            print("You have guessed the word, the word was %s" % words)
     else:
         guesses_left -= 1
         print("You are wrong!")
 
 if guesses_left == 0:
-    print("The word was %s" % newStr)
+    print("The correct word was %s" % newStr)
 
-if their_guess == newStr:
-    guesses_left = 0
-    print("You guessed the word. The word was %s" % newStr)
+
+# if their_guess == output:
+# guesses_left = 0
+# print("You guessed the word. The word was %s" % newStr)
