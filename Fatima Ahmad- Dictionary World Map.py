83300d9f4e08a81d165f2d893a14828836168308
Fahmad920 <31050480+Fahmad920@users.noreply.github.com>
2018-02-13 15:39:33 -0800

    1/13/18
    Finished Map

--- Fatima Ahmad- Dictionary World Map.py
+++ Fatima Ahmad- Dictionary World Map.py
@@ -1,4 +1,4 @@
-#world_map = {
+# world_map = {
 #     'WESTHOUSE':{
 #         'NAME': 'WEST OF HOUSE',
 #         'DESCRIPTION': 'You are west of a small house',
@@ -28,144 +28,151 @@
 
 # at least 15 rooms
 # Brave setting
-# obstables: witch , Mordu, Wilo-the-wisp
+# obstacles: witch , Mordu, Wilo-the-wisp
 
 
 brave_map = {
-     'MERIDAS ROOM':{
-         'NAME': 'MERIDAS ROOM',
-         'DESCRIPTION': 'Welcome to Meridas room! In here there is a bow and arrow, a sword, and a sack.',
-         'There is a door North, South, East, and West of the room.'
-         'PATHS': {
-             'WEST': 'KITCHEN',
-             'SOUTH': 'DINING ROOM',
-             'EAST': 'PARENTS ROOM'
-         }
-    },
-    'PARENTSROOM':{
-        'NAME': 'PARENTSROOM',
-        'DESCRIPTION':'This is where the king and queen stay.',
-                      'There is a dresser in the room and a door to the East.'
+    'MERIDASROOM': {
+        'NAME': 'MERIDAS ROOM',
+        'DESCRIPTION': 'Welcome to Meridas room! In here there is a bow and arrow, a sword, and a sack. \n'
+                       'There is a door North, South, East, and West of the room.',
         'PATHS': {
-            'EAST': 'MERIDASROOM'
+            'WEST': 'KITCHEN',
+            'SOUTH': 'DINING ROOM',
+            'EAST': 'PARENTS ROOM'
         }
     },
-    'DININGROOM':{
-        'NAME': 'DININGROOM',
-        'DESCRIPTION': 'There is a table in the middle of the room.',
-        'There is a shield on the wall and a bear statue in the corner.'
-        'There is a door to the north.'
+    'PARENTSROOM': {
+        'NAME': 'PARENTS ROOM',
+        'DESCRIPTION': 'This is where the king and queen stay.\n'
+                       'There is a dresser in the room and a door to the West.',
         'PATHS': {
-            'NORTH': 'MERIDASROOM',
+            'WEST': 'MERIDASROOM'
+        }
+    },
+    'DININGROOM': {
+        'NAME': 'DINING ROOM',
+        'DESCRIPTION': 'There is a table in the middle of the room.\n'
+                       'There is a shield on the wall and a bear statue in the corner.\n'
+                       'There is a door to the north.',
+        'PATHS': {
+            'WEST': 'MERIDAS ROOM',
             'DOWN': 'SECRET ROOM'
         }
     },
     'KITCHEN': {
         'NAME': 'KITCHEN',
-        'DESCRIPTION': 'There is a door going north.',
-        'In the kitchen, there is a cake on the table and a box.'
-        'There is a little crate on the floor next to the door.'
-        'PATHS':{
+        'DESCRIPTION': 'There is a door that leads North and West.\n'
+                       'In the kitchen, there is a cake on the table and a box.\n'
+                       'There is a little crate on the floor next to the door.',
+        'PATHS': {
             'WEST': 'STABLES',
             'NORTH': 'GATE'
         }
     },
     'OUTSIDE': {
         'NAME': 'OUTSIDE',
-        'DESCRIPTION': 'Out here is the main gate.',
-        'West to the gate are the stables.'
-        'The gate out leads to the forest.'
+        'DESCRIPTION': 'Out here is the main gate.\n'
+                       'West to the gate are the stables.\n'
+                       'The gate out leads to the forest.',
         'PATHS': {
             'NORTH': 'FOREST',
             'WEST': 'STABLES',
             'SOUTH': 'KITCHEN'
-         }
+        }
     },
     'FIGHTINGAREA': {
-        'NAME': 'FIGHTINGAREA',
-        'DESCRIPTION': 'Here are lots of weapons.',
-        'Off to the North there is water and some boats'
-        'PATHS':{
+        'NAME': 'FIGHTING AREA',
+        'DESCRIPTION': 'Here are lots of weapons.\n'
+                       'Off to the North there is water and some boats',
+        'PATHS': {
             'NORTH': 'OUTSIDE',
             'SOUTH': 'WATER'
         }
     },
     'STABLES': {
         'NAME': 'STABLES',
-        'DESCRIPTION':'Here are all the horses.',
-        'There is hay and water here as well'
+        'DESCRIPTION': 'Here are all the horses.\n'
+                       'There is hay and water here as well.',
         'PATHS': {
             'SOUTH': 'STABLES',
             'WEST': 'OUTSIDE'
-     }
+        }
     },
     'FOREST': {
         'NAME': 'FOREST',
-        'DESCRIPTION': 'There is a path to the south.',
-        'There is a path that leads to the North and to the East'
+        'DESCRIPTION': 'There is a path to the south.\n'
+                       'There is a path that leads to the North and to the East',
         'PATHS': {
             'SOUTH': 'FIREFALL',
             'EAST': 'MAZE',
             'NORTH': 'OUTSIDE'
         }
     },
-    'FIREFALL':{
+    'FIREFALL': {
         'NAME': 'FIREFALL',
-        'DESCRIPTION': 'Here there is a water fall and a rock to climb.',
-        'Legends say that only kings were brave enough to drink from this water fall.'
-        'PATHS':{
+        'DESCRIPTION': 'Here there is a water fall and a rock to climb.\n'
+                       'Legends say that only kings were brave enough to drink from this water fall.',
+        'PATHS': {
             'NORTH': 'FOREST',
             'EAST': 'THE RING OF STONES'
         }
     },
-    'THE RING OF STONES':{
+    'THE RING OF STONES': {
         'NAME': 'THE RING OF STONES',
-        'DESCRIPTION': 'Here are stones that are placed in a circular pattern.',
-        'People have said that The Ring of Stones tends to take you places to change your fate.'
-        'PATHS':{
+        'DESCRIPTION': 'Here are stones that are placed in a circular pattern.\n'
+                       'People have said that The Ring of Stones tends to take you places to change your fate.',
+        'PATHS': {
             'EAST': 'WITCHES COTTAGE',
             'NORTH': 'FOREST',
             'WEST': 'FIRE FALL'
         }
     },
-    'WITCHES COTTAGE':{
+    'WITCHES COTTAGE': {
         'NAME': 'WITCHES COTTAGE',
-        'DESCRIPTION': 'You have found the witches ',
-        'Inside you find many wood carvings, but a special carving in the back catches your eye.'
-        'PATHS':{
+        'DESCRIPTION': 'You have found the witches.\n'
+                       'Inside you find many wood carvings, but a special carving in the back catches your eye.',
+        'PATHS': {
             'WEST': 'THE RING OF STONES',
             'DOWN': 'MAGIC ROOM',
             'UP': 'DINING ROOM'
         }
     },
-    'MAGIC ROOM':{
+    'MAGIC ROOM': {
         'NAME': 'SECRET MAGIC ROOM',
-        'DESCRIPTION': 'This is where the witch does her magic.',
-        'The only way out is from the door you came in from.'
+        'DESCRIPTION': 'This is where the witch does her magic.\n'
+                       'The only way out is from the door you came in from.',
         'PATHS': {
             'UP': 'WITCHES COTTAGE'
         }
     },
-    'RIVER':{
+    'RIVER': {
         'NAME': 'RIVER',
-        'DESCRIPTION': 'There is a river that runs off into two separate rivers.',
-        'Here you see some bears catching fish.'
-        'There is a path that leads north'
+        'DESCRIPTION': 'There is a river that runs off into two separate rivers.\n'
+                       'Here you see some bears catching fish.\n'
+                       'There is a path that leads north',
         'PATH': {
             'NORTH': 'THE RING OF STONE',
             'EAST': 'ANCIENT KINGDOM RUINS'
         }
     },
-    'ANCIENT KINGDOM RUINS':{
+    'ANCIENT KINGDOM RUINS': {
         'NAME': 'ANCIENT KINGDOM RUINS',
-        'DESCRIPTION': 'This is the old kingdom of Dunbroch before Mordu destroyed it.',
-        'Here you find the same symbol you seen in the castle of the three bears in a circular pattern.'
-        'There is only one path that leads down.'
-    'PATHS':{
-        'DOWN': 'MORDUS CAVE'
-    }
-
+        'DESCRIPTION': 'This is the old kingdom of Dunbroch before Mordu destroyed it.\n'
+                       'You find the same symbol you seen in the castle of the three bears in a circular pattern.\n'
+                       'There is only one path that leads down.',
+        'PATHS': {
+            'DOWN': 'MORDUS CAVE'
+        }
     },
-    ''
-
+    'MORDUS CAVE': {
+        'NAME': 'MORDUS CAVE',
+        'DESCRIPTION': 'Your in a dark cave with only faint light from above.\n'
+                       'There are bones everywhere, and many broken weapons.\n'
+                       'There is also a tapestry of four princes.\n'
+                       'You hear heavy breathing behind you, and you see Mordu behind you.',
+        'PATHS': {
+            'UP': 'MORDUS CAVE'
+        }
+    }
 }
