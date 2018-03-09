7d0973c5240105b576099bdb240ce5ec7b6b1b24
Fahmad920 <31050480+Fahmad920@users.noreply.github.com>
2018-03-09 15:41:57 -0800

    3/9/18

--- Fatima Ahmad- Character.py
+++ Fatima Ahmad- Character.py
@@ -1,20 +1,21 @@
-import random
-
-
 class Character(object):
-    def __init__(self, name, description, item, health, damage=10):
+    def __init__(self, name, description, item, health, location, tree, damage=10):
         self.name = name
         self.description = description
         self.inventory = []
         self.item = item
         self.health = health
         self.damage = damage
-
+        self.alive = False
+        self.location = location
+        self.tree = tree
 
     def look(self):
+        print(self.location.name)
         print("You have looked around")
 
     def climb(self):
+        print(self.tree.location)
         print("You have looked around")
 
     def take(self, item):
@@ -22,21 +23,38 @@ class Character(object):
         print("You have taken something")
 
     def health(self):
+        print(self.name.damage)
         print("You have health")
 
     def take_damage(self, amt):
+        if self.health <= 0:
+            print("%s is already dead" % self.name)
+            return
         self.health -= amt
+        if self.health <= 0:
+            self.alive = False
+            print("%s has died." % self.name)
 
     def attack(self, target):
         if self.alive:
+            print("%s attacks %s. %s's health is %d. The enemy's health is %d." % (self.name, target.name, self.name,
+                                         self.health,
+                                                                                   target.health))
             target.take_damage(self.damage)
         else:
             print("%s is dead and cannot attack" % self.name)
 
-player = Character("You", "Description", 100, None, None, 20)
-enemy = Character("Enemy", "It's an enemy", 100, None, None)
+
+player = Character("name", "description", None, 100, None, 10)
+enemy = Character("Enemy", "It's an enemy", 100, None, None, 10)
 
 player.attack(enemy)
+player.attack(enemy)
+player.attack(enemy)
+player.attack(enemy)
+player.attack(enemy)
+player.attack(enemy)
+
 enemy.attack(player)
-print(player.health)
-print(enemy.health)
+print("%s's health is %s." % (player.name, player.health))
+print("%s's health is %s." % (enemy.name, enemy.health))
